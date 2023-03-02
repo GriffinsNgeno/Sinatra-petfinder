496be2284f3fac6b1011afbf02539ccd313c3103
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-02 14:00:29 +0300

    update

--- app/application_contoller.rb
+++ app/application_contoller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < Sinatra::Base
 
     get '/' do
-      
+        { message: "Hello world" }.to_json
     end
-  
+    
+    get '/owner' do
+        owner = Owner.all.order(:name).limit(10)
+        owner.to_json
+      end
+
+      get '/animal/:id' do
+        animal = Animal.find(params[:id])
+        animal.to_json
+        animal.to_json(only: [:id, :image, :name, :age, :breed, :gender], include: {
+            owner: { only: [:name] }
+          } 
+        )
+    end
   end
