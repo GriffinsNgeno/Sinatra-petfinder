320227470eece98735ca3e7f7b4ff3353598e97c
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-03 13:23:41 +0300

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@ puts "🌱 Seeding data..."
 50.times do
   Owner.create(name: Faker::Name.name)
 end
-rand(1..50).times do
+rand(1..10).times do
 owner = Owner.order('RANDOM()').first
 # Make 10 animals
-100.times do
+50.times do
     Animal.create(
         image: Faker::LoremFlickr.image(size: "120x150", search_terms: ['animal']),
         name: Faker::Creature::Animal.name,
