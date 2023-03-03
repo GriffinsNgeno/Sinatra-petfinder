4454a02d5f404163986722829462ca2eade97cec
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-03 13:30:01 +0300

    Updated application _controller

--- app/application_contoller.rb
+++ app/application_contoller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json' 
 
-    get '/' do
-        { message: "Hello world" }.to_json
-    end
-    
     get '/owner' do
-        owner = Owner.all.order(:name).limit(10)
+        owner = Owner.all
         owner.to_json
+      end
+
+      get '/animal' do
+        animal = Animal.all.sort
+        animal.to_json
       end
 
       get '/animal/:id' do
@@ -14,7 +16,34 @@ class ApplicationController < Sinatra::Base
         animal.to_json
         animal.to_json(only: [:id, :image, :name, :age, :breed, :gender], include: {
             owner: { only: [:name] }
-          } 
-        )
+          })
     end
+
+    delete '/animal/:id' do
+      animal = Animal.find(params[:id])
+      animal.destroy
+      animal.to_json
+      end
+
+      post '/animal' do
+        animal = Animal.create(
+          image: params[:image],
+          name: params[:name],
+          age: params[:age],
+          breed: params[:breed],
+          gender: params[:gender]
+        )
+        animal.to_json
+      end
+
+      patch '/animal/:id' do
+        animal = Animal.find(params[:id])
+        animal.update(
+          name: params[:name],
+          age: params[:age],
+          breed: params[:breed],
+          gender: params[:gender]
+        )
+        animal.to_json
+      end
   end
